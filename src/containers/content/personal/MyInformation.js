3203a7c1cc10f1d0c1a4efd67fade8ff63ea615c
左旺 <zuowang@zuowangdeMacBook-Pro.local>
2019-01-23 21:56:37 +0800

    修改首页布局

--- src/containers/content/personal/MyInformation.js
+++ src/containers/content/personal/MyInformation.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import css from './MyInformation.module.scss'
-import { Icon } from 'antd';
+import { Icon, Divider } from 'antd';
 
 export class MyInformation extends Component {
   static propTypes = {
@@ -14,35 +14,104 @@ export class MyInformation extends Component {
     return (
       <div className={css.informationCss}>
         <div className={css.topContentCss}>
-          <div className={`${css.headCss} ${userType}`}>
-          </div>
-          <div className={css.basicInfoCss}>
-            <div className={css.username}>
-              <div>
-                <span className={css.nameText}>项洁洁</span> (技术人员)
+          <div className={css.lfContentCss}>
+            <div className={`${css.headCss} ${userType}`}>
+            </div>
+            <div className={css.basicInfoCss}>
+              <div className={css.username}>
+                <div>
+                  <span className={css.nameText}>项洁洁</span> (技术人员)
+                </div>
+                <div className={css.editCss}>
+                  <Icon type='edit'></Icon> 修改
+                </div>
               </div>
-              <div className={css.editCss}>
-                <Icon type='edit'></Icon> 修改
+              <div className={css.line}></div>
+              <div className={css.userdept}>
+                技术中心-前端部-前端副组长
               </div>
             </div>
-            <div className={css.line}></div>
-            <div className={css.userdept}>
-              技术中心-前端部-前端副组长
+          </div>
+          <div className={css.rtContentCss}>
+            <div>
+            <Icon className={css.iconCss} type='user'></Icon> 工号： L1988
+            </div>
+            <div>
+            <Icon className={css.iconCss}  type='phone'></Icon> 手机号码： 18755489161
+            </div>
+            <div>
+            <Icon className={css.iconCss}  type='mail'></Icon> 邮箱： 18755489161@163.com
             </div>
           </div>
         </div>
         <div className={css.botContentCss}>
-          <div>
-          <Icon type='user'></Icon> 工号： L1988
+          <div className={css.titleCss}>
+              <div>
+                <Divider type="vertical" style={{background: '#555'}} />基本信息
+              </div>
+              <div className={css.flexContent}>
+                <div>
+                  年龄：21
+                </div>
+                <div>
+                  民族：汉
+                </div>
+                <div>
+                  地址：皖安庆桐城市孔城镇陶泥村万庄队
+                </div>
+              </div>
+              <div className={css.flexContent}>
+                <div>
+                  年龄：21
+                </div>
+                <div>
+                  民族：汉
+                </div>
+                <div>
+                  地址：皖安庆桐城市孔城镇陶泥村万庄队
+                </div>
+              </div>
+              <div className={css.flexContent}>
+                <div>
+                  年龄：21
+                </div>
+                <div>
+                  民族：汉
+                </div>
+                <div>
+                  地址：皖安庆桐城市孔城镇陶泥村万庄队
+                </div>
+              </div>
+              <div>
+                <Divider type="vertical"  style={{background: '#555'}}  />教育信息
+              </div>
+              <div className={css.flexContent}>
+                <div>
+                  年龄：21
+                </div>
+                <div>
+                  民族：汉
+                </div>
+                <div>
+                  地址：皖安庆桐城市孔城镇陶泥村万庄队
+                </div>
+              </div>
+              <div className={css.flexContent}>
+                <div>
+                  年龄：21
+                </div>
+                <div>
+                  民族：汉
+                </div>
+                <div>
+                  地址：皖安庆桐城市孔城镇陶泥村万庄队
+                </div>
+              </div>
+              {/* <Divider orientation="left" style={{fontSize: '13px', color: '#999'}}>基本信息：</Divider> */}
           </div>
-          <div>
-          <Icon type='phone'></Icon> 手机号码： 18755489161
-          </div>
-          <div>
-          <Icon type='email'></Icon> 邮箱： 18755489161@163.com
-          </div>
-          </div>
+        </div>
       </div>
+     
     )
   }
 }
--- src/containers/content/personal/Personal.js
+++ src/containers/content/personal/Personal.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import styles from './Personal.module.scss'
 
-
 import MyInformation from './MyInformation'
 import MyAttendance from './MyAttendance'
+import { Divider } from 'antd';
 
 export class Personal extends Component {
   static propTypes = {
@@ -16,8 +16,10 @@ export class Personal extends Component {
   render() {
     return (
       <div className={styles.personalCss}>
+        {/* <Divider orientation="left" style={{fontSize: '13px', color: '#999'}}>个人资料</Divider> */}
         <div className="title">个人资料</div>
         <MyInformation></MyInformation>
+        {/* <Divider orientation="left" style={{fontSize: '13px', marginTop: '50px', color: '#999'}}>数据总览</Divider> */}
         <div className="title">数据总览</div>
         <MyAttendance></MyAttendance>
       </div>
